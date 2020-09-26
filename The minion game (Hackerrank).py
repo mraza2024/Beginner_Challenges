d5a060ba0bec8bd6432ebb6aee6624ea6467e6bc
Ammar Raza <68449684+mraza2024@users.noreply.github.com>
2020-09-26 22:43:31 +0300

    Add files via upload

---
+++ The minion game (Hackerrank).py
@@ -0,0 +1,24 @@
+s = input()
+s = s.upper()
+
+vowels = "AEIOU"
+
+stuart_score = 0
+kevin_score = 0
+
+
+for i in range(len(s)):
+
+    if s[i] in vowels:
+        kevin_score += (len(s)-i)
+    else:
+        stuart_score += (len(s)-i)
+
+if kevin_score > stuart_score:
+    print ("Kevin", kevin_score)
+
+elif kevin_score < stuart_score:
+    print ("Stuart", stuart_score)
+
+else:
+    print ("Draw")
